ae38b5ff28ba5e3cf4ac07288089272d8b27a5ac
Alex Iats <chava160389@gmail.com>
2023-01-22 18:20:47 +0000

    feat: :sparkles: done with GET, DELETE, PUT. Project finished

--- app.js
+++ app.js
@@ -3,25 +3,37 @@ const path = require('path')
 const {v4} = require('uuid');
 const app = express();
 
-const CONTACTS = [
+let CONTACTS = [
     {id: v4(), name: "Alejandro", value: '+346706544444', marked: false}
 ]
 
-api.use(express.json())
+app.use(express.json())
 
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS)
 })
 
 app.post('/api/contacts', (req, res) => {
-    console.log(req.body);
-    res.json({test: 1})
+   const contact = { ... req.body, id: v4(), marked: false }
+   CONTACTS.push(contact);
+   res.status(201).json(contact);
 })
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: "Contact have been deleted"})
+})
+
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
 })
 
 app.listen(3000, () => console.log('server start'))
--- client/frontend.js
+++ client/frontend.js
@@ -19,14 +19,20 @@ const app = createApp({
     methods: {
         async createContact() {
             const {...contact} = this.form;
-            const response = await request('/api/contacts', 'POST', contact)
-            console.log(response);
+            const newContact = await request('/api/contacts', 'POST', contact);
+            this.contacts.push(newContact);
+            this.form.name = this.form.value = '';
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(el => el.id === id);
-            contact.marked = true;
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            })
+            contact.marked = updated.marked
         },
-        removeContact(id) {
+        async removeContact(id) {
+            request(`/api/contacts/${id}`, "DELETE")
             this.contacts = this.contacts.filter(c => c.id !== id)
         }
     },
